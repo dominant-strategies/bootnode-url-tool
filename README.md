d5cba205f6f2eb2901fad6a38a91103cec5506da
Alan Orwick <alan.kev11@gmail.com>
2023-09-27 16:36:02 -0500

    Add useful script and update README

--- README.md
+++ README.md
@@ -7,6 +7,25 @@ $ go build .
 
 #### Creating bootnode keys
 
+#### If you wish to generate nodeurls for a single key.
+Generate the nodeurls for a given bootkey.txt
+
 ```shell
-$ ./quai-bootnode-util ~/Projects/dev-ops/ansible/bootkeys/**/* > nodeurls.txt 
+$ ./quai-bootnode-util bootnode.key > nodeurls.txt 
+```
+#### If you wish to gneerate nodeurls for many keys.
+Create the keys.yml file that you wish to read bootnode keys off of.
+
+```shell
+$ cp keys.yml.dist keys.yml
+```
+
+Convert the keys.yml into a folder of nodekeys.
+```shell
+$ ./convert.sh
+```
+
+Convert the nodekeys into a folder of nodeurls.txt.
+```shell
+$ ./create_enodes.sh
 ```
---
+++ create_enodes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Directory containing the nodekey files
+key_dir="keys"
+
+# Check if key directory exists
+if [[ ! -d $key_dir ]]; then
+    echo "Directory $key_dir not found!"
+    exit 1
+fi
+
+# Create enodes directory if it doesn't exist
+mkdir -p enodes
+
+# Iterate over each IP directory inside the key_dir
+for folder in "$key_dir"/*; do
+    # Ensure it's a directory
+    if [[ -d "$folder" ]]; then
+        # Extract IP from the folder name
+        ip=$(basename "$folder")
+
+        echo "Processing IP: $ip"
+
+        # Ensure IP specific directory exists in enodes
+        mkdir -p "enodes/$ip"
+
+        # Run the quai-bootnode-util command and redirect output to the respective enode file
+        ./quai-bootnode-util "$folder/nodekey" > "enodes/$ip/enode.txt"
+    fi
+done
+
+echo "Enode files have been created in the enodes/ directory."
